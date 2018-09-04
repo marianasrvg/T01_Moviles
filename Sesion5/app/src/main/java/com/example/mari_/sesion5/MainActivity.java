15d669e2993cbb22486ca0e70f682c74865f0c99
Mariana Sierra <sierravgmr@gmail.com>
2018-09-04 07:30:46 -0500

    First try

---
+++ Sesion5/app/src/main/java/com/example/mari_/sesion5/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mari_.sesion5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id==R.id.action_bar_btn_save){
+            Toast.makeText(this, "help 01", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+}
